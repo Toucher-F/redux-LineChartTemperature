c42f2af721907ce13ca0bcd411854c5f22785553
Toucher-F <lianduanb@gmail.com>
2017-12-18 21:31:45 -0500

    bind function debug

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,14 @@
+import axios from "axios";
 const API_KEY = "d8ed7c781e897f6750df05c5562a294b";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
 
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ca`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
