08a9d55cfb80f6fe59dbe56c20c327cbef09ad4e
Toucher-F <lianduanb@gmail.com>
2017-12-19 16:43:53 -0500

    fix wrong city bug

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,8 +2,11 @@ import { FETCH_WEATHER } from "../actions/index";
 
 export default function(state = [], action) {
   switch (action.type) {
-    case FETCH_WEATHER:
+    case FETCH_WEATHER: {
+      if (!action.payload.data) return state;
+
       return [action.payload.data, ...state];
+    }
   }
 
   return state;
